a3a00dd9fc9b2ae64dc787409d51c391afaa3c68
giangnct <giangnct@mobio.io>
2023-11-25 14:56:19 +0700

    update data

--- gendata.py
+++ gendata.py
@@ -10,8 +10,12 @@ male_names = import_data_from_file("./data/name/male.dat")
 lastnames = import_data_from_file("./data/name/lastname.dat")
 cities = import_data_from_file("./data/location/city.dat")
 occupations = import_data_from_file("./data/occupation.dat")
-print(random.choice(lastnames) + " " + random.choice(female_names))
-
+# print(random.choice(lastnames) + " " + random.choice(female_names))
+age = ["18-23","23-30","30+"]
 profile = {
-    "name" =
+    "name": random.choice(lastnames) + " " + random.choice(male_names),
+    "age": random.choice(age),
+    "city": random.choice(cities),
+    "occupations": random.choice(occupations)
 }
+print(profile)
